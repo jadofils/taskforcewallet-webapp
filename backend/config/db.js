c8d83ce5cf593da1984d51384412aedd8df830aa
unknown <sezikeyejadofils100@gmail.com>
2025-01-21 04:00:05 +0200

    deuplicate removed

--- backend/config/db.js
+++ backend/config/db.js
@@ -31,10 +31,7 @@ const connectDB = async () => {
             throw new Error('MONGO_URI is not defined in environment variables');
         }
 
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
